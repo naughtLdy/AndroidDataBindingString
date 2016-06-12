bd1c0eb729ea7184d3aa44addbdb196f7c3dd5df
naught <sdax05@gmail.com>
2016-06-12 19:43:28 +0900

    add binding data

--- app/src/main/java/com/example/naught/androiddatabindingstring/MainActivity.java
+++ app/src/main/java/com/example/naught/androiddatabindingstring/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.naught.androiddatabindingstring;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.naught.androiddatabindingstring.databinding.ActivityMainBinding;
+import com.example.naught.androiddatabindingstring.models.User;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
     }
 }
